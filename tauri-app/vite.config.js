c6d32a05e235100b65cf2e4abe0771f125443501
RomanAtBrief <roman@thebrief.space>
2025-11-30 19:27:33 +0000

    Tauri and taildwind works and builds correctly.

--- tauri-app/vite.config.js
+++ tauri-app/vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
     target:
       process.env.TAURI_ENV_PLATFORM == 'windows'
         ? 'chrome105'
-        : 'safari13',
+        : 'es2022',
     // don't minify for debug builds
     minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
